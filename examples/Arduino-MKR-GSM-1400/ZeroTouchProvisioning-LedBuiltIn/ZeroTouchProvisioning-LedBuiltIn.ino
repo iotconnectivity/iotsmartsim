b1849af18a5ef02ce8c231ad24e4ee7dbe8652ed
J. Félix Ontañón <felix.ontanon@podgroup.com>
2021-05-31 18:22:09 +0200

    Added MIT License

--- examples/Arduino-MKR-GSM-1400/ZeroTouchProvisioning-LedBuiltIn/ZeroTouchProvisioning-LedBuiltIn.ino
+++ examples/Arduino-MKR-GSM-1400/ZeroTouchProvisioning-LedBuiltIn/ZeroTouchProvisioning-LedBuiltIn.ino
@@ -1,17 +1,12 @@
 /*
-  IoTSoundSensor. Copyright (c) 2021 Pod Group Ltd. http://podgroup.com
+  Arduino MKR GSM 1400 Zero Touch Provisioning Demo. 
+  Copyright (c) 2021 Pod Group Ltd. http://podgroup.com
   
   This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License version 3 as
-  published by the Free Software Foundation
-  
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-  
-  You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  it under the terms of the MIT License.
+
+  You should have received a copy of the MIT License
+  along with this program.  If not, see <https://opensource.org/licenses/MIT>.
 
   Description:
     - Powers ON/OFF The LED_BUILTIN for Arduino MKR 1400 GSM board.
